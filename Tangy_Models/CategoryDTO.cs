5a3cfcb28a1726e918a820eff231d3d0b6322368
litvinovvn <LitvinovVN@rambler.ru>
2022-02-14 23:01:57 +0300

    76

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Tangy_Models
     public class CategoryDTO
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите наименование")]
         public string Name { get; set; }
     }
 }
